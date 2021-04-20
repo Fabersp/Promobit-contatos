2bdd933a1a172adf9790dd0a31f15b16abfd6a00
Monitora <monitora@Fabricio-Mac-mini.local>
2021-04-19 21:10:24 -0300

    Insert dos Pods

---
+++ Pods/Target Support Files/Pods-Promobit-Contatos-Promobit-ContatosUITests/Pods-Promobit-Contatos-Promobit-ContatosUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Promobit_Contatos_Promobit_ContatosUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Promobit_Contatos_Promobit_ContatosUITests
+@end
---
+++ Pods/Target Support Files/Pods-Promobit-ContatosTests/Pods-Promobit-ContatosTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Promobit_ContatosTests : NSObject
+@end
+@implementation PodsDummy_Pods_Promobit_ContatosTests
+@end
